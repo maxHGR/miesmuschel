9f88224167a6440fb8ea3683921595b199c2f4fd
maxHGR <maxhgr2@gmail.com>
2023-02-26 17:32:54 +0100

    "updated css and component"

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,67 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-import clam from '../public/images/clam.png';
+import { useState, useEffect } from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
 
-  function requestAnswer() {
-    const nonce = Math.floor(Math.random() * 5);
-    let answer;
+  const [isOpen, setIsOpen] = useState(false);
+  const [popupContent, setPopupContent] = useState('');
 
-    switch(nonce) {
-      case 0:
-        answer = "Auf jeden Fall !";
-        break;
-      case 1:
-        answer = "Ich glaube eher nicht...";
-        break;
-      case 2:
-        answer = "Ja";
-        break;
-      case 3:
-        answer = "Nein";
-        break;
-      case 4:
-        answer = "Frag doch einfach nochmal";
-        break;
-    }
+    
+  
+    const handlePopupOpen = () => {
+      
+        const nonce = Math.floor(Math.random() * 5);
+        let result;
+    
+        switch(nonce) {
+          case 0:
+            result = "Auf jeden Fall !";
+            break;
+          case 1:
+            result = "Ich glaube eher nicht...";
+            break;
+          case 2:
+            result = "Ja";
+            break;
+          case 3:
+            result = "Nein";
+            break;
+          case 4:
+            result = "Frag doch einfach nochmal";
+            break;
+        }
+        
+        setPopupContent(result);
+      
+    };
 
-    window.confirm(answer);
-  }
+    const overlayStyle = {
+      backgroundImage: 'url("/images/flowers.webp")',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center center',
+    };
 
+    const style = {
+      backgroundColor: 'rgba(255, 160, 122, 0.7)',
+    };
+ 
   return (
     <>
       <main>
-          <button onClick={requestAnswer}>
-            Magische Miesmuschel...
-          </button>
+        <button onClick={() => setIsOpen(true)}>Click me</button>
+        <Popup 
+          open={isOpen} 
+          onClose={() => setIsOpen(false)} 
+          onOpen={handlePopupOpen}
+          overlayStyle={overlayStyle}
+          style={style}
+        >
+          <div>{popupContent}</div>
+        </Popup>
       </main>
     </>
   )
